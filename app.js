46f36214aa5b699e65be39f9e37011c2ce031ac6
Sabbir-Rahman <sabbirrahman42@iut-dhaka.edu>
2022-02-12 05:09:24 +0600

    refresh token implemented

--- app.js
+++ app.js
@@ -23,6 +23,35 @@ const posts = [
     }
 ]
 
+//store locally now can be store in redis, mongo or etc
+let refreshTokens = [] 
+
+// refresh token route
+app.post('/token',(req,res)=> {
+    const refreshToken = req.body.token
+
+    if (refreshToken == null) res.status(403).json({'message':'Token not given'})
+    if(!refreshTokens.includes(refreshToken)) return res.status(403).json({'message':'Token not valid'})
+
+    jwt.verify(refreshToken,process.env.REFRESH_TOKEN_SECRET, (err,user) => {
+        if (err) return res.status(403).json({'message':'Token not valid'})
+        
+        //remove prev refresh token if you want 
+
+
+        const accessToken = generateAccessToken(user)
+
+        res.json({ accessToken: accessToken })
+    })
+
+})
+
+app.delete('/logout', (req,res)=> {
+    refreshTokens = refreshTokens.filter(token => token !== req.body.token)
+    res.sendStatus(204)
+})
+
+
 app.get('/posts',authenticateToken ,(req,res) => {
     console.log(req.user)
     res.json(posts)
@@ -33,8 +62,12 @@ app.post('/login', (req,res) => {
     const username = req.body.username
     const user = { name:username }
 
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-    res.json({ accessToken: accessToken})
+    const accessToken = generateAccessToken(user)
+    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+
+    refreshTokens.push(refreshToken)
+
+    res.json({ accessToken: accessToken, refreshToken: refreshToken })
 })
 
 function authenticateToken(req, res, next) {
@@ -48,6 +81,10 @@ function authenticateToken(req, res, next) {
         next()
     })
 
+}
+
+function generateAccessToken(user) {
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '35s'})
 }
 
 app.listen(port,()=>{
